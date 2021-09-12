51fc8a3637dbd64779fd5d4c5f4db191b5308858
Felix de Maneville <felix.maneville@gmail.com>
2021-09-12 19:38:36 +0200

    3D examples

---
+++ examples/3d_cloth.rs
@@ -0,0 +1,84 @@
+use bevy::prelude::*;
+use bevy_verlet::{
+    BevyVerletPlugin, VerletLocked, VerletPoint3D, VerletPointPbrBundle, VerletStick,
+};
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BevyVerletPlugin::default())
+        .insert_resource(WindowDescriptor {
+            width: 1000.,
+            height: 800.,
+            ..Default::default()
+        })
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-50., 0., -50.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+    let material = materials.add(Color::WHITE.into());
+    let fixed_material = materials.add(Color::RED.into());
+    let mesh = meshes.add(Mesh::from(shape::Cube::new(1.)));
+    let stick_length: f32 = 2.;
+    let (origin_x, origin_y) = (-5., 10.);
+    let (points_x_count, points_y_count) = (30, 5);
+    let mut entities = Vec::new();
+    for j in 0..points_y_count {
+        for i in 0..points_x_count {
+            let mut cmd = commands.spawn_bundle(VerletPointPbrBundle {
+                verlet_point: VerletPoint3D::new(Vec3::new(
+                    origin_x + (2.0 * i as f32),
+                    origin_y + (2.0 * j as f32),
+                    0.,
+                )),
+                pbr_bundle: PbrBundle {
+                    mesh: mesh.clone(),
+                    material: material.clone(),
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+            cmd.insert(Name::new(format!("Point {}", i)));
+            if j == 0 {
+                cmd.insert(VerletLocked {}).insert(fixed_material.clone());
+            }
+            entities.push(cmd.id());
+        }
+    }
+    for (i, entity) in entities.iter().enumerate() {
+        let above = i.checked_sub(points_x_count);
+        let left = if i % points_x_count == 0 {
+            None
+        } else {
+            Some(i - 1)
+        };
+        spawn_stick(&mut commands, *entity, &entities, stick_length, above);
+        spawn_stick(&mut commands, *entity, &entities, stick_length, left);
+    }
+}
+
+fn spawn_stick(
+    commands: &mut Commands,
+    entity: Entity,
+    entities: &[Entity],
+    length: f32,
+    coord: Option<usize>,
+) {
+    if let Some(i) = coord {
+        let other_entity = entities.get(i).unwrap();
+        commands.spawn().insert(VerletStick {
+            point_a_entity: entity,
+            point_b_entity: *other_entity,
+            length,
+        });
+    }
+}
---
+++ examples/3d_line.rs
@@ -0,0 +1,115 @@
+use bevy::prelude::*;
+use bevy_verlet::{
+    BevyVerletPlugin, VerletLocked, VerletPoint3D, VerletPointPbrBundle, VerletStick,
+};
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BevyVerletPlugin::default())
+        .insert_resource(WindowDescriptor {
+            width: 1000.,
+            height: 500.,
+            ..Default::default()
+        })
+        .add_startup_system(setup_camera.system())
+        .add_startup_system(setup_free_line.system())
+        .add_startup_system(setup_fixed_line.system())
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-30., 4., -80.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn setup_free_line(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let material = materials.add(Color::WHITE.into());
+    let fixed_material = materials.add(Color::RED.into());
+    let mesh = meshes.add(Mesh::from(shape::Cube::new(1.)));
+    let stick_length: f32 = 1.;
+    let points_count = 10;
+    let mut previous_entity = None;
+    for i in 0..=points_count {
+        let mut cmd = commands.spawn_bundle(verlet_bundle(
+            material.clone(),
+            mesh.clone(),
+            Vec3::new(i as f32, 20., 0.),
+        ));
+        cmd.insert(Name::new(format!("Point {}", i)));
+        if previous_entity.is_none() {
+            cmd.insert(VerletLocked {}).insert(fixed_material.clone());
+        }
+        let entity = cmd.id();
+        if let Some(e) = previous_entity {
+            commands
+                .spawn()
+                .insert(VerletStick {
+                    point_a_entity: e,
+                    point_b_entity: entity,
+                    length: stick_length,
+                })
+                .insert(Name::new(format!("Stick {}", i)));
+        }
+        previous_entity = Some(entity);
+    }
+}
+
+fn setup_fixed_line(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let material = materials.add(Color::WHITE.into());
+    let fixed_material = materials.add(Color::RED.into());
+    let mesh = meshes.add(Mesh::from(shape::Cube::new(1.)));
+    let stick_length: f32 = 1.;
+    let points_count = 20;
+    let start_pos = -10.;
+    let mut previous_entity = None;
+    for i in 0..=points_count {
+        let mut cmd = commands.spawn_bundle(verlet_bundle(
+            material.clone(),
+            mesh.clone(),
+            Vec3::new(start_pos + i as f32, 0., 0.),
+        ));
+        cmd.insert(Name::new(format!("Point {}", i)));
+        if previous_entity.is_none() || i == points_count {
+            cmd.insert(VerletLocked {}).insert(fixed_material.clone());
+        }
+        let entity = cmd.id();
+        if let Some(e) = previous_entity {
+            commands
+                .spawn()
+                .insert(VerletStick {
+                    point_a_entity: e,
+                    point_b_entity: entity,
+                    length: stick_length,
+                })
+                .insert(Name::new(format!("Stick {}", i)));
+        }
+        previous_entity = Some(entity);
+    }
+}
+
+fn verlet_bundle(
+    material: Handle<StandardMaterial>,
+    mesh: Handle<Mesh>,
+    pos: Vec3,
+) -> VerletPointPbrBundle {
+    VerletPointPbrBundle {
+        verlet_point: VerletPoint3D::new(pos),
+        pbr_bundle: PbrBundle {
+            mesh,
+            material,
+            ..Default::default()
+        },
+        ..Default::default()
+    }
+}
