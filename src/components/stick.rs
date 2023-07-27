12839e71797475d04471c74fcbd98799f4f35cd4
Félix Lescaudey de Maneville <felix.maneville@gmail.com>
2023-07-27 09:52:57 +0200

    Performance improvements (#12)

--- src/components/stick.rs
+++ src/components/stick.rs
@@ -12,3 +12,12 @@ pub struct VerletStick {
     /// Target stick length
     pub length: f32,
 }
+
+impl VerletStick {
+    #[inline]
+    #[must_use]
+    /// Returns [`point_a_entity`, `point_b_entity`]
+    pub const fn entities(&self) -> [Entity; 2] {
+        [self.point_a_entity, self.point_b_entity]
+    }
+}
